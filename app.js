126208816d48d3e59d78bf5feb70c7e7b4af6bee
cgaswin <cgaswin333@gmail.com>
2023-05-24 22:29:09 +0530

    working on authentication

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require("express")
-
+const cors = require("cors")
+const morgan = require("morgan")
 const app = express()
+
+//regular middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+
+app.use(cors)
+app.use(morgan("tiny"))
+
 
 
 module.exports = app
